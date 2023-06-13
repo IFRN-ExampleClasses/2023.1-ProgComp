97cb6996ec62c1976acb94f62f9613c31a013457
charles-freitas <ccmfreitas@gmail.com>
2023-06-13 16:55:48 -0300

    Atualização de Repositório

---
+++ 2023-06-13 - LISTAS/exemplo_01_capitais_ne.py
@@ -0,0 +1,9 @@
+siglas = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE']
+
+capitais = ['Maceió', 'Salvador', 'Fortaleza', 'São Luís', 'João Pessoa', 
+            'Recife', 'Teresina', 'Natal', 'Aracaju']
+
+
+populacap = [1018948, 2872347, 2669342, 1101884, 809015, 1695727,
+             868075, 890480, 657013]
+            
